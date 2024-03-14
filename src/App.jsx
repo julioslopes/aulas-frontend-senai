9e6123f1926ffc262c50a17c5225daea44efcdf6
JULIO CESAR DOS SANTOS LOPES <julio_s_lopes@estudante.sesisenai.org.br>
2024-03-14 19:35:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  console.log('App render...');
+
+  return (
+    <>
+      <header></header>
+      <aside></aside>
+      <main></main>
+      <footer></footer>
+    </>
+  );
+}
+
+export default App;
